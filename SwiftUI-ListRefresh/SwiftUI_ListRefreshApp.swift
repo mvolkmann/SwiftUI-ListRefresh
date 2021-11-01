edf358bc82bc33a42073881b42ebf6e07cf0cae1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-01 10:16:24 -0500

    Initial Commit

---
+++ SwiftUI-ListRefresh/SwiftUI_ListRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListRefreshApp.swift
+//  SwiftUI-ListRefresh
+//
+//  Created by R. Mark Volkmann on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
