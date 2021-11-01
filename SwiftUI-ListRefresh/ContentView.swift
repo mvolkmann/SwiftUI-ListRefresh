54edb3f84cec55c8309d92470a58d79e3fa11930
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-01 10:49:16 -0500

    initial version

--- SwiftUI-ListRefresh/ContentView.swift
+++ SwiftUI-ListRefresh/ContentView.swift
@@ -1,16 +1,51 @@
-//
-//  ContentView.swift
-//  SwiftUI-ListRefresh
-//
-//  Created by R. Mark Volkmann on 11/1/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    struct Dog: CustomStringConvertible, Identifiable, Hashable {
+        let id = UUID()
+        let name: String
+        let breed: String
+        var description: String { "\(name) - \(breed)" }
+    }
+
+    @State private var dogs = [
+        Dog(name: "Maisey", breed: "Treeing Walker Coonhound"),
+        Dog(name: "Ramsay", breed: "Native American Indian Dog"),
+        Dog(name: "Oscar", breed: "German Shorthaired Pointer"),
+        Dog(name: "Comet", breed: "Whippet")
+    ]
+
+    @State private var selectedIds = Set<UUID>()
+    
+    func loadMore() async {
+        // This calls a REST service that returns nothing after 2 seconds.
+        let url = URL(string: "https://httpbin.org/delay/2")!
+        let request = URLRequest(url: url)
+        // Not using return value.
+        let _ = try! await URLSession.shared.data(for: request)
+        
+        dogs.append(Dog(name: "Clarice", breed: "Whippet"))
+        dogs.append(Dog(name: "Vixen", breed: "Whippet"))
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            NavigationView {
+                List(dogs, selection: $selectedIds) { dog in
+                    let desc = String(describing: dog)
+                    if selectedIds.contains(where: {$0 == dog.id}) {
+                        Text(desc).bold().foregroundColor(.green)
+                    } else {
+                        Text(desc)
+                    }
+                }
+                .navigationTitle("Dogs")
+                // The EditButton in the toolbar toggles
+                // the edit mode of the NavigationView.
+                .toolbar { EditButton() }
+            }.refreshable { await loadMore() }
+            Text("\(selectedIds.count) selections")
+        }
     }
 }
 
